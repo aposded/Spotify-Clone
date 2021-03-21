250c3039d383922abf8251009668b9dd1d4c7726
Apostolos Dedeloudis <apostolosded@gmail.com>
2021-03-21 23:10:39 +0200

    n

--- client/src/useAuth.jsx
+++ client/src/useAuth.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default function useAuth() {
-    
+export default function useAuth(code) {
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresIn] = useState()
+
+    useEffect(() => {
+        axios.post('http://localhost:3001/login', {code}).then( res => {
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, '/')
+        }).catch((err) => {
+            window.location = '/'
+        })
+    }, [code])
+
+    useEffect(() => {
+        
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const cors = require('cors')
 const SpotifyWebApi = require('spotify-web-api-node')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '250a9153ab334c6cba6723476cf799eb',
+        clientSecret: 'a4405b6610f94bcb97bd3c26c6af5575',
+        refreshToken
+    })
+})
+
+spotifyApi.refreshAccessToken().then(
+    data => {console.log(data.body)
+        spotifyApi.setAccessToken(data.body['access_token'])
+    }).catch(() => {
+        res.sendStatus(400)
+    })
 
 app.post('/login', (req, res) => {
     const code = req.body.code
@@ -20,3 +41,5 @@ app.post('/login', (req, res) => {
         })
     })
 })
+
+app.listen(3001)
